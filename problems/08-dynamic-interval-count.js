d72a8ea960d2443bb0200d19ceb153fc5a1955ff
HusseinEid <husseingeid@gmail.com>
2020-11-19 14:21:50 -0800

    problem 8

--- problems/08-dynamic-interval-count.js
+++ problems/08-dynamic-interval-count.js
@@ -1,13 +1,13 @@
 /***********************************************************************
 Write a function `dynamicIntervalCount` that accepts a callback, delay
-in milliseconds, and an optional amount as arguments. The function should 
-set an interval with the given callback and delay. If an amount argument 
-is passed, the interval should be cleared after the callback has been 
+in milliseconds, and an optional amount as arguments. The function should
+set an interval with the given callback and delay. If an amount argument
+is passed, the interval should be cleared after the callback has been
 called 'amount' number of times. If an amount argument is not passed,
 the interval should not be cleared and `dynamicIntervalCount` should instead
 return the Timeout object for the interval.
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Examples:
@@ -24,11 +24,36 @@ const timeoutObject = intervalCount(function() {
 console.log(timeoutObject); // Timeout { ... }
 ***********************************************************************/
 
+function dynamicIntervalCount(cb, delay, num) {
+    if (num === undefined) {
+        return setInterval(cb, delay)
+    }
+    const interval = setInterval(function() {
+        cb()
+        num--
 
+        if (num === 0) {
+            clearInterval(interval)
+        }
+    }, delay)
+    return interval
+}
+
+
+// dynamicIntervalCount(function() {
+//     console.log('hi');
+// }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
+
+
+// const timeoutObject = dynamicIntervalCount(function() {
+//     console.log('hi');
+// }, 500); // prints 'hi' at 500ms intervals indefinitely
+
+// console.log(timeoutObject); // Timeout { ... }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
-  module.exports = dynamicIntervalCount;
+    module.exports = dynamicIntervalCount;
 } catch {
-  module.exports = null;
+    module.exports = null;
 }
